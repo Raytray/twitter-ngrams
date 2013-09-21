e7050f04a8b730ba59757eb0aabe138fb87b46d1
Raymond Tang <raymond@tang.me>
2013-09-21 15:35:37 -0400

    change return of find to be OrderedDict
    * Plays better with pandas/vincent

--- mongo_query.py
+++ mongo_query.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient, ASCENDING
+from collections import OrderedDict
 
 
 client = MongoClient()
@@ -7,18 +8,15 @@ mongo_coll.ensure_index('text', ASCENDING)
 
 
 def find(token=None):
-    return_list = []
+    return_dict = {}
     text_token = "text.{}".format(token)
     if token:
         for item in mongo_coll.find({text_token: {'$exists': True}}):
-            return_list.append({'date': item['_id'],
-                                'text': token,
-                                'percentage': (item['text'][token]/
-                                               float(sum(item['text'].values()))*100)})
+            return_dict[item['_id']] = {'text': token,
+                                       'percentage': (item['text'][token]/
+                                                      float(sum(item['text'].values()))*100)}
     else:
         for item in mongo_coll.find():
-            return_list.append({'date': item['_id'],
-                                'text': sum(item['text'].values())
-                                })
+            return_dict[item['_id']] = {'text': sum(item['text'].values())}
 
-    return return_list
+    return OrderedDict(sorted(return_dict.items(), key=lambda t: t[0]))
